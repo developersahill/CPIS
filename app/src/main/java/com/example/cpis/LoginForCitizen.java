6240a83b847b77283ebfda94ee674f2b70e00cda
developersahill <79632946+developersahill@users.noreply.github.com>
2021-06-04 11:20:26 +0530

    Splash Screen Done

---
+++ app/src/main/java/com/example/cpis/LoginForCitizen.java
@@ -0,0 +1,14 @@
+package com.example.cpis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginForCitizen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_for_citizen);
+    }
+}
---
+++ app/src/main/java/com/example/cpis/SignUpforCitizen.java
@@ -0,0 +1,15 @@
+package com.example.cpis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpforCitizen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_upfor_citizen);
+
+    }
+}
